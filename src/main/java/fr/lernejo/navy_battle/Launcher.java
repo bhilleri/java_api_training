1c9f67d6f85a74d0590dd1d2bcb43800c235f6fb
benjamin <benjaminh@live.fr>
2021-12-03 10:58:44 +0100

    Setup Maven

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,14 @@
+package fr.lernejo.navy_battle;
+
+/**
+ * Classe contenant le Main
+ */
+public class Launcher {
+    /**
+     * Point d'entrée dans le programme
+     */
+    public static void main(String [] args)
+    {
+        Launcher launcher =new Launcher();
+    }
+}
