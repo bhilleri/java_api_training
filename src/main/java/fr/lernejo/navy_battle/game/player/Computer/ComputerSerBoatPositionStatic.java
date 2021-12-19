97e5801d8bd83e67c44da2c2e57d0987bc5aba5e
bhilleri <benjaminh@live.fr>
2021-12-20 00:20:33 +0100

    Set initialization first

---
+++ src/main/java/fr/lernejo/navy_battle/game/player/Computer/ComputerSerBoatPositionStatic.java
@@ -0,0 +1,21 @@
+package fr.lernejo.navy_battle.game.player.Computer;
+
+import fr.lernejo.navy_battle.game.point.IPoint;
+import fr.lernejo.navy_battle.game.point.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerSerBoatPositionStatic implements IComputerSetBoatPosition{
+    final List<Integer> numbers = new ArrayList<>();
+    @Override
+    public List<IPoint> PositionABoat(int size) {
+        List <IPoint> pointList = new ArrayList<>();
+        for (int i = 0; i <size; i++)
+        {
+            pointList.add(new Point(numbers.size(),i));
+        }
+        numbers.add(1);
+        return pointList;
+    }
+}
